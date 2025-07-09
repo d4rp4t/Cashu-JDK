bd5058904364a8bcc7cc0ce3dc300d3652c6d294
lollerfirst <lollerfirst@gmail.com>
2025-07-09 10:19:18 +0200

    add coinselection method

---
+++ src/main/java/com/project/utils/CoinSelection.java
@@ -0,0 +1,11 @@
+package com.project.utils;
+
+import java.util.List;
+
+import com.project.nut00.Proof;
+
+public class CoinSelection {
+    public static void selectProofsToSend(List<Proof> inProofs, List<Proof> outSendProofs, List<Proof> outKeepProofs) {
+        
+    } 
+}
