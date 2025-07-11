8b3f3255d97ea459b7bdfafd4f458525d0391288
d4rp4t <d4rp4t@tutanota.com>
2025-07-11 12:27:35 +0200

    idk why keysetid was in bytes

--- src/main/java/com/cashujdk/nut00/InnerToken.java
+++ src/main/java/com/cashujdk/nut00/InnerToken.java
@@ -1,11 +1,10 @@
 package com.cashujdk.nut00;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 //Inner token of v4 token. Has to belong to the same mint
 public class InnerToken {
-    public byte[] keysetId;
+    public String keysetId;
     public List<Proof> proofs;
 }
