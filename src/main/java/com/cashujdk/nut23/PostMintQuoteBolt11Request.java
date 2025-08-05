8772c6901a8e2d50f070b9caee86b0931433b3d5
d4rp4t <d4rp4t@tutanota.com>
2025-08-05 13:07:41 +0200

    Nut20

--- src/main/java/com/cashujdk/nut23/PostMintQuoteBolt11Request.java
+++ src/main/java/com/cashujdk/nut23/PostMintQuoteBolt11Request.java
@@ -1,11 +1,17 @@
 package com.cashujdk.nut23;
 
 import com.cashujdk.nut04.PostMintQuoteRequest;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Optional;
 
 public class PostMintQuoteBolt11Request extends PostMintQuoteRequest {
 
     public long amount;
     public String description;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public Optional<String> pubkey; // not specified in constructor, can be added manually since its public
 
     public PostMintQuoteBolt11Request(String unit, long amount, String description) {
         super(unit);
--- src/main/java/com/cashujdk/nut23/PostMintQuoteBolt11Response.java
+++ src/main/java/com/cashujdk/nut23/PostMintQuoteBolt11Response.java
@@ -1,6 +1,9 @@
 package com.cashujdk.nut23;
 
 import com.cashujdk.nut04.PostMintQuoteResponse;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Optional;
 
 public class PostMintQuoteBolt11Response extends PostMintQuoteResponse {
 
@@ -8,11 +11,15 @@ public class PostMintQuoteBolt11Response extends PostMintQuoteResponse {
     public String state;
     public long expiry;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public Optional<String> pubkey;
+
     public PostMintQuoteBolt11Response(String unit, String quote, String request, long amount, String state, long expiry) {
         super(quote, request, unit);
         this.amount = amount;
         this.state = state;
         this.expiry = expiry;
     }
+
 
 }
